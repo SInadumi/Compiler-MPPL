7aec1e7d43b54ac9e507956ff6221fbd4825758f
SInadumi <inazumi19@icloud.com>
2020-10-18 19:28:47 +0900

    ADD Print_File

--- token-list.c
+++ token-list.c
@@ -10,18 +10,19 @@ const char *tokenstr[NUMOFTOKEN+1] = {
 	">=", "(", ")", "[", "]", ":=", ".", ",", ":", ";", "read","write", "break"
 };
 char Error_msg[MAXSTRSIZE];
+static int numtoken[NUMOFTOKEN+1];		/* Token counter */
+static void Print_File(char *name);
 
 int main(int nc, char *np[]) {
 	int token;
-	int numtoken[NUMOFTOKEN+1];		/* Token counter */
 
 	/* File Open */
     if(nc < 2) {
 		sprintf(Error_msg, "File name is not given.\n");
 		error(Error_msg);
 		return 0;
-		printf("HI");
     }
+
     if(init_scan(np[1]) < 0) {
 		sprintf(Error_msg, "File %s can not open.\n", np[1]);
 		error(Error_msg);
@@ -35,15 +36,34 @@ int main(int nc, char *np[]) {
 		/*if(token == TNAME)*/
     }
     end_scan();
-	//printf("%d",get_linenum());
+
     /* Print */
-	for(int i = 0; i <= NUMOFTOKEN; i++){
-		if(numtoken[i] == 0) continue;
-		printf("\"%s\"\t", tokenstr[i]);
-		printf("%d\n", numtoken[i]);
-	}
+	Print_File(np[1]);
 
     return 0;
+}
+
+/*
+	Output result in output/[name]
+*/
+void Print_File(char *name){
+	FILE *output;
+	char filename[MAXSTRSIZE];
+
+	sprintf(filename, "outputs/%s",name);
+	output = fopen(filename, "w");
+
+	if(output == NULL){
+		error("file doesn't exist");
+		fclose(output);
+		return ;
+	}
+
+	for(int i = 0; i <= NUMOFTOKEN; i++){
+		if(numtoken[i] == 0) continue;
+		fprintf(output, "\"%s\t\"\t%d\n", tokenstr[i], numtoken[i]);
+	}
+	fclose(output);
 }
 
 void error(char *mes) {
